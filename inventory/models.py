75b0ce3451377a52ddd471463ce74a830d5ee336
pillager86 <pillager86@localhost.localdomain>
2022-07-15 21:30:30 -0400

    wip Order model for DB

--- inventory/models.py
+++ inventory/models.py
@@ -3,6 +3,7 @@ from django.core.validators import MinLengthValidator
 from django.utils.translation import gettext_lazy
 
 class Item(models.Model):
+    # possibly move this outside?
     class Department(models.TextChoices):
         DAIRY = "DAI", gettext_lazy('Dairy')
         FROZEN = "FRO", gettext_lazy('Frozen')
@@ -16,9 +17,21 @@ class Item(models.Model):
     case_cost = models.DecimalField(decimal_places=2, max_digits=6)
     full_price = models.DecimalField(decimal_places=2, max_digits=6)
     member_price = models.DecimalField(decimal_places=2, max_digits=6)
-    department = models.CharField(max_length=3, choices=Department.choices, default=Department.DAIRY)
+    department = models.CharField(max_length=3, choices=Department.choices)
     qoh = models.IntegerField(default=0)
 
     def __str__(self):
         return self.upc + " " + self.name + " (" + self.get_department_display() + ")"
+
+class Order(models.Model):
+    department = models.CharField(max_length=3, choices=Item.Department.choices)
+    date_of_creation = models.DateField()
+    submitted = models.BooleanField()
+    date_of_submission = models.DateTimeField()
+
+class OrderEntry(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    upc = models.ForeignKey(Item, on_delete=models.CASCADE)
+    number_of_cases = models.IntegerField(default=1)
+
 
